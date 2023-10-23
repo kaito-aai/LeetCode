69c0aead328e40dbe1b24bd6838ae87c24df4638
kaito-aai <kaito.s0508@gmail.com>
2023-10-23 16:56:04 +0000

    solved 111

---
+++ Javascript/111.minimum-depth-of-binary-tree.js
@@ -0,0 +1,38 @@
+/*
+ * @lc app=leetcode id=111 lang=javascript
+ *
+ * [111] Minimum Depth of Binary Tree
+ */
+
+function TreeNode(val, left, right) {
+     this.val = (val===undefined ? 0 : val)
+     this.left = (left===undefined ? null : left)
+     this.right = (right===undefined ? null : right)
+}
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var minDepth = function(root) {
+    if (root === null) {
+        return 0;
+    }
+    if (root.left === null) {
+        return minDepth(root.right) + 1;
+    }
+    if (root.right === null) {
+        return minDepth(root.left) + 1;
+    }
+    return Math.min(minDepth(root.right), minDepth(root.left)) + 1;
+};
+// @lc code=end
