1a59afd7599db6cd67b486ef942a0ad349e3a0b2
kaito-aai <kaito.s0508@gmail.com>
2023-10-31 11:09:39 +0000

    fixed 141

--- Javascript/141.linked-list-cycle.js
+++ Javascript/141.linked-list-cycle.js
@@ -18,17 +18,18 @@
  * @return {boolean}
  */
 var hasCycle = function(head) {
-    let currentTarget = head;
-    const checked = [];
+    let fast = head;
+    let slow = head;
 
-    while (currentTarget !== null) {
-        if (checked.some((v) => v === currentTarget)) {
+    while (fast !== null && fast.next !== null) {
+        fast = fast.next.next;
+        slow = slow.next;
+
+        if (fast === slow) {
             return true;
         }
-        
-        checked.push(currentTarget);
-        currentTarget = currentTarget.next;
     }
+
     return false;
 };
 // @lc code=end
