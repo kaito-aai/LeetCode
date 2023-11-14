79dc51af74cbf72dae895ec02816f7abff37b335
kaito-aai <kaito.s0508@gmail.com>
2023-11-14 10:29:55 +0900

    solved 226

---
+++ Javascript/226.invert-binary-tree.js
@@ -0,0 +1,36 @@
+/*
+ * @lc app=leetcode id=226 lang=javascript
+ *
+ * [226] Invert Binary Tree
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+var invertTree = function(root) {
+    if (root === null || (root.left === null & root.right === null)) {
+        return root;
+    }
+
+    const left = root.left;
+
+    root.left = root.right;
+    root.right = left;
+
+    invertTree(root.left);
+    invertTree(root.right);
+
+    return root;
+};
+// @lc code=end
+
