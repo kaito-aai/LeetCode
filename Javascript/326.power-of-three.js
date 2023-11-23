4148886bb899d772a5842a1f359bf7702ce1cc9d
kaito-aai <kaito.s0508@gmail.com>
2023-11-24 00:14:18 +0900

    fixed 326

--- Javascript/326.power-of-three.js
+++ Javascript/326.power-of-three.js
@@ -10,15 +10,12 @@
  * @return {boolean}
  */
 var isPowerOfThree = function(n) {
-    if (n < 1) {
-        return false;
-    }
     while (n > 1) {
         n /= 3;
         if (!Number.isInteger(n)) {
             return false;
         }
     }
-    return true;
+    return n === 1;
 };
 // @lc code=end
