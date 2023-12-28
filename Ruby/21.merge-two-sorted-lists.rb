94897eb9e0f3e5dac111c3300863ebdb5c4c2564
kaito-aai <kaito.s0508@gmail.com>
2023-12-28 19:50:28 +0000

    ruby: fixed 21

--- Ruby/21.merge-two-sorted-lists.rb
+++ Ruby/21.merge-two-sorted-lists.rb
@@ -6,42 +6,26 @@
 
 # @lc code=start
 # Definition for singly-linked list.
-class ListNode
-    attr_accessor :val, :next
-    def initialize(val = 0, _next = nil)
-        @val = val
-        @next = _next
-    end
-end
+# class ListNode
+#     attr_accessor :val, :next
+#     def initialize(val = 0, _next = nil)
+#         @val = val
+#         @next = _next
+#     end
+# end
 # @param {ListNode} list1
 # @param {ListNode} list2
 # @return {ListNode}
 def merge_two_lists(list1, list2)
-    head = ListNode.new
-    current = head
-    current1 = list1
-    current2 = list2
+    return list2 if list1.nil?
+    return list1 if list2.nil?
 
-    while current1 != nil || current2 != nil
-        if current1 == nil
-            current.next = current2
-            current2 = current2.next
-        elsif current2 == nil
-            current.next = current1
-            current1 = current1.next
-        elsif current2.val < current1.val
-            current.next = current2
-            current2 = current2.next
-        else current1.val < current2.val
-            current.next = current1
-            current1 = current1.next
-        end
-
-        current = current.next
+    if list1.val < list2.val
+        list1.next = merge_two_lists(list1.next, list2)
+        return list1
+    else 
+        list2.next = merge_two_lists(list1, list2.next)
+        return list2
     end
-
-    return head.next
 end
 # @lc code=end
-
-merge_two_lists(ListNode.new(1, ListNode.new(2, ListNode.new(4))), ListNode.new(1, ListNode.new(3, ListNode.new(4))))
