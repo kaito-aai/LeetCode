6bde73b66cdad5d1cf6303f9689485aaf501cba2
kaito-aai <kaito.s0508@gmail.com>
2024-01-06 09:13:22 +0900

    javascript: fixed 500

--- Javascript/500.keyboard-row.js
+++ Javascript/500.keyboard-row.js
@@ -10,21 +10,10 @@
  * @return {string[]}
  */
 var findWords = function(words) {
-    const output = [];
-    const rows = ["qwertyuiop", "asdfghjkl", "zxcvbnm"];
-    let wordsIndex = 0;
-    while (wordsIndex < words.length) {
-        for (let r = 0; r < rows.length; r++) {
-            if (Array.from(words[wordsIndex]).every(v => rows[r].includes(v.toLowerCase()))) {
-                output.push(words[wordsIndex]);
-                break;
-            }
-        }
-
-        wordsIndex++;
-    }
-
-    return output;
+    return words.filter(w =>
+        /^[qwertyuiop]*$/i.test(w) ||
+        /^[asdfghjkl]*$/i.test(w) ||
+        /^[zxcvbnm]*$/i.test(w));
 };
 // @lc code=end
 
